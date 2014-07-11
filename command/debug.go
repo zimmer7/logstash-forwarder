e2292dce8083fab7774d91314a3bf1ad3f496626
Joubin Houshyar <joubin.houshyar@elasticsearch.com>
2014-07-10 22:31:50 -0400

    FIX WIP option handling for commands (gneral)
    MOD lsf debug

--- command/debug.go
+++ command/debug.go
@@ -18,52 +18,35 @@
 package command
 
 import (
-	"log"
+	"github.com/elasticsearch/kriterium/errors"
+	"github.com/elasticsearch/kriterium/flags"
 	"lsf"
 )
 
 const cmd_debug lsf.CommandCode = "debug"
 
 var Debug *lsf.Command
-var debugOptions *debugOptionsSpec
-
-type debugOptionsSpec struct {
-	command StringOptionSpec
-}
+var debugOption *flags.StringOption
 
 func init() {
 
+	flagset := FlagSet(cmd_debug)
 	Debug = &lsf.Command{
 		Name:  cmd_debug,
 		About: "Provides usage information for LS/F commands",
+		Init:  initDebug,
 		Run:   runDebug,
-		Flag:  FlagSet(cmd_debug),
+		Flag:  flagset,
 		Usage: "debug <command>",
 	}
+	debugOption = flags.NewStringOption(flagset, "c", "command", "", "the command to debug", true)
+}
 
-	debugOptions = &debugOptionsSpec{
-		command: NewStringOptionSpec("c", "command", "", "the command you need debug with", true),
-	}
-	debugOptions.command.defineFlag(Debug.Flag)
+func initDebug(env *lsf.Environment, args ...string) error {
+	return flags.VerifyRequiredOption(debugOption)
 }
 
 func runDebug(env *lsf.Environment, args ...string) error {
-
-	debug("env.bound: %t", env.IsBound())
-	debug0(env, "system.create-time")
-	debug0(env, "stream.apache-123.scan.blocksize-byte")
-
-	return nil
-}
-
-func debug0(env *lsf.Environment, record string) {
-	value, e := env.GetRecord(record)
-	if e != nil {
-		debug("env.GetRecord(\"%s\") - NOT FOUND", record)
-	} else {
-		debug("env.GetRecord(\"%s\") => %q", record, string(value))
-	}
-}
-func debug(fmt string, args ...interface{}) {
-	log.Printf("command.Debug.debug: "+fmt, args...)
+	//	_ = debugOption.Get()
+	return errors.NotImplemented("command:", cmd_debug)
 }
